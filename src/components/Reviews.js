5044143b08ea6c45723cb8d4f4af6ea957f587b1
Valeriy Gogin <qbarez@yandex.ru>
2023-02-01 19:55:27 +0300

    add routes

---
+++ src/components/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reviews.css';
+
+function Reviews () {
+  return (
+    <div className="reviews">
+      <p>Здесь будут ревью!</p>
+    </div>
+  )
+}
+
+export default Reviews;
