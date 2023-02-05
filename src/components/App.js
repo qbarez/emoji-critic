be463a926d039d2c6753e0238dcf304204168a56
Valeriy Gogin <qbarez@yandex.ru>
2023-02-05 22:11:56 +0300

    feat: add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Dashboard from './Dashboard';
 import Reviews from "./Reviews";
@@ -10,24 +10,41 @@ import { Route, Routes } from 'react-router-dom';
 import MyStory from "./about-me/MyStory";
 import Hobbies from "./about-me/Hobbies";
 import Contact from "./about-me/Contact";
-import SiteHistory from "./about-us/SiteHistory";
-import SiteMission from "./about-us/SiteMission";
+import SiteHistory from './about-us/SiteHistory';
+import SiteMission from './about-us/SiteMission';
+import Review from "./Review";
+import PageNotFound from './PageNotFound';
 
 function App() {
+  const [reviews, setReviews] = useState();
+  // этот хук используется единожды, при запуске компонента
+  useEffect(() => {
+    // получаем данные с сервера
+    fetch('https://api.nomoreparties.co/emoji-critic-rus').then((res) => {
+      return res.json();
+    }).then((parsedReviews) => {
+      // форматируем данные и, используя setData, обновляем текущий стейт
+      setReviews(Object.values(parsedReviews));
+    })
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews}/>} />
+        <Route exact path="/reviews/:id" element={<Review reviews={reviews}/>} />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="my-story" element={<MyStory />} />
           <Route path="hobbies" element={<Hobbies />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-        <Route path="/about-us" element={<AboutUs />} >
+        <Route path="/about-us" element={<AboutUs />}>
           <Route path="site-history" element={<SiteHistory />} />
           <Route path="site-mission" element={<SiteMission />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />}>
         </Route>
       </Routes>
     </div>
--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -5,13 +5,12 @@ import {Outlet, Link} from "react-router-dom";
 function AboutUs () {
   return (
     <div className="about-us">
-      <h2>О нас</h2>
       <ul className="links">
         <li>
-        <Link to={`site-history`}>История приложения</Link>
+          <Link to={`site-history`}>История приложения</Link>
         </li>
         <li>
-        <Link to={`site-mission`}>Наша миссия</Link>
+          <Link to={`site-mission`}>Наша миссия</Link>
         </li>
       </ul>
       <Outlet />
